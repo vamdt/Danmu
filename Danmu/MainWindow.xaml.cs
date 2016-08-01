cbb07c7f3381a25f5903c5b7ddc64e7a6b17ae00
vamdt <liang6688267@gmail.com>
2016-08-01 23:59:25 +0800

    Working on draw textblock

--- Danmu/MainWindow.xaml.cs
+++ Danmu/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,15 +29,43 @@ namespace Danmu
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-           
+            linesCount = ((int)(Playground.ActualHeight - paddingTop)) % lineHeight;
         }
 
-            Random rand = new Random();
+        private double paddingTop = 5;
+        private int lineHeight = 16;
+        private int linesCount;
+
+        private Random rand = new Random();
+
+        private double DanmuYPosition(int lineNumber)
+        {
+            return paddingTop + lineHeight * (lineNumber - 1);
+        }
+
+        private int RandomLineNumber()
+        {
+            return rand.Next(linesCount);
+        }
+
+
         private void Shoot()
         {
             TextBlock danmu = new TextBlock();
             danmu.Text = "23333333333333333333333333333333333333";
-            danmu.Margin = new Thickness(0, rand.NextDouble() * 500, 0, 0);
+            danmu.TextWrapping = TextWrapping.Wrap;
+            double y = DanmuYPosition(RandomLineNumber());
+            danmu.Text += " linesCount:" +  linesCount;
+            danmu.Text += " position:" + y.ToString();
+            danmu.Text += " height:" + danmu.RenderSize.Height;
+            danmu.Text += " width:" + danmu.RenderSize.Width;
+            danmu.Margin = new Thickness(0, y, 0, 0);
+            danmu.LineHeight = lineHeight;
+            danmu.Background = Brushes.Red;
+            Size danmuSize = MeasureText(danmu);
+            danmu.Height = danmuSize.Height;
+            danmu.Width = danmuSize.Width;
+            danmu.LineStackingStrategy = LineStackingStrategy.BlockLineHeight;
             Playground.Children.Add(danmu);
             DoubleAnimation animation = new DoubleAnimation();
             animation.From = Playground.RenderSize.Width;
@@ -47,9 +76,21 @@ namespace Danmu
             transform.BeginAnimation(TranslateTransform.XProperty, animation);
         }
 
+        private Size MeasureText(TextBlock textBlock)
+        {
+            FormattedText formattedText = new FormattedText(
+                textBlock.Text,
+                CultureInfo.CurrentUICulture,
+                FlowDirection.LeftToRight,
+                new Typeface(textBlock.FontFamily, textBlock.FontStyle, textBlock.FontWeight, textBlock.FontStretch),
+                textBlock.FontSize,
+                Brushes.Black);
+            return new Size(formattedText.Width, formattedText.Height);
+        }
+
         private void PlayButton_Click(object sender, RoutedEventArgs e)
         {
-            for(int i =0; i<10; i++)
+            for (int i = 0; i < 4; i++)
             {
                 Shoot();
             }
